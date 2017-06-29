f5510d4087de40a567023f4099a67e7750b38afa
oscarf <k3ther@gmail.com>
2017-06-29 23:11:00 +0200

    Deleted old code

--- src/main/scala/sparkTry.scala
+++
@@ -1,8 +0,0 @@
-val nums = Array((1 to 50):_*)
-val oneRDD = sc.parallelize(nums)
-oneRDD.count
-val otherRDD = oneRDD.map(_ * 2)
-val result = otherRDD.collect
-for (v <- result) println(v)
-val sum = oneRDD.reduce(_ + _)
-print(sum)
