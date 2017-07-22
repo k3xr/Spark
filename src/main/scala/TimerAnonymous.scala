41dda8a6a5eccc7308af9d94f86c2c168f98ecfe
oscarf <k3ther@gmail.com>
2017-07-22 12:13:22 +0200

    Added anonymous function example

---
+++ src/main/scala/TimerAnonymous.scala
@@ -0,0 +1,16 @@
+/**
+  * Anonymous function example
+  */
+object TimerAnonymous {
+
+    def oncePerSecond(callback: () => Unit) {
+        while (true) {
+            callback(); Thread sleep 1000
+        }
+    }
+
+    def main(args: Array[String]) {
+        oncePerSecond(() =>
+            println("time flies like an arrow..."))
+    }
+}
