e92041487cdfd4497379374fe9b0f9de25374ee3
oscarf <k3ther@gmail.com>
2017-07-28 23:35:58 +0200

    Style

--- src/main/scala/Complex.scala
+++ src/main/scala/Complex.scala
@@ -1,11 +1,11 @@
 /**
-  * Scala class example
-  * @param real
-  * @param imaginary
+  * Scala Complex class example
+  * @param real real component
+  * @param imaginary imaginary component
   */
 class Complex(real: Double, imaginary: Double) {
-    def re() = real
-    def im() = imaginary
-    override def toString() =
+    def re(): Double = real
+    def im(): Double = imaginary
+    override def toString: String =
         "" + re + (if (im < 0) "" else "+") + im + "i"
 }
