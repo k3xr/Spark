24fd641daf8cdfb3c0b47eb9191477ff8517a7d1
oscarf <k3ther@gmail.com>
2017-08-19 14:28:42 +0200

    Format

--- src/main/scala/Complex.scala
+++ src/main/scala/Complex.scala
@@ -6,6 +6,5 @@
 class Complex(real: Double, imaginary: Double) {
     def re(): Double = real
     def im(): Double = imaginary
-    override def toString: String =
-        "" + re + (if (im < 0) "" else "+") + im + "i"
+    override def toString: String = "" + re + (if (im < 0) "" else "+") + im + "i"
 }
