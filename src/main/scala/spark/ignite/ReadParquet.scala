616b3dfe3a2c8e59252e2f1a5a6583b392d782ec
k3xr <k3ther@gmail.com>
2017-10-06 13:32:48 +0200

    Added example parquet ignite code

---
+++ src/main/scala/spark/ignite/ReadParquet.scala
@@ -0,0 +1,69 @@
+package spark.ignite
+
+import org.apache.ignite.spark.{IgniteContext, IgniteRDD}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.util.Random
+
+/**
+  * Example writes data to parquet file, reads the file
+  * and saves it in ignite.
+  */
+object ReadParquet {
+  def main(args: Array[String]): Unit = {
+    // Spark Configuration
+    val conf = new SparkConf()
+      .setAppName("IgniteRDDExample")
+      .setMaster("local")
+      .set("spark.executor.instances", "2")
+
+    // Spark context
+    val sparkContext = new SparkContext(conf)
+
+    val spark = SparkSession
+      .builder()
+      .config(conf)
+      .getOrCreate()
+
+    import spark.implicits._
+
+    // Adjust the logger to exclude the logs of no interest.
+    Logger.getRootLogger.setLevel(Level.ERROR)
+    Logger.getLogger("org.apache.ignite").setLevel(Level.INFO)
+
+    // Defines spring cache Configuration path.
+    val CONFIG = "example-shared-rdd.xml"
+
+    Random.setSeed(6789)
+    val ngaussRDD = sparkContext.parallelize(1 to 100000, 10).map(i => (i, Random.nextGaussian.toInt))
+    val ngaussDF = ngaussRDD.toDF()
+
+    ngaussDF.write.parquet("data.parquet")
+
+    val parquetAssAccType = spark.read.parquet("data.parquet").rdd
+
+    // Creates Ignite context with above configuration.
+    val igniteContext = new IgniteContext(sparkContext, CONFIG, false)
+
+    // Creates an Ignite Shared RDD of Type (Int,Int) Integer Pair.
+    val sharedRDD: IgniteRDD[Int, Int] = igniteContext.fromCache[Int, Int]("sharedRDD")
+
+    // RDD a RDD[Pair[String,String]]
+    val rddPairs:org.apache.spark.rdd.RDD[(Int, Int)] =
+      parquetAssAccType.map(r => (r.getInt(0), r.getInt(1)))
+
+    sharedRDD.savePairs(rddPairs)
+
+    val sharedRDD2 = igniteContext.fromCache("associatedAccountType")
+
+    val first = sharedRDD2.take(1)
+    println(first)
+
+    igniteContext.close(true)
+
+    // Stop SparkContext.
+    sparkContext.stop()
+  }
+}
